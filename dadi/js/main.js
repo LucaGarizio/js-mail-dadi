cb9aa7ecb0dd88df77e846a85a3d62caa5e2d568
Luca Garizio <garizioluca2@gmail.com>
2023-11-22 16:28:34 +0100

    ex DADI:  style : row & cols html/css

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -2,23 +2,45 @@
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-// Generare un numero random da 1 a 6 per il giocatore 
-const userNumber = Math.floor(Math.random() * 6) +1;
-console.log(userNumber);
 
-// Generare un numero random da 1 a 6 per il computer. 
-const pcNumber = Math.floor(Math.random() * 6) +1;
-console.log(pcNumber);
 
-// Stabilire il vincitore, in base a chi fa il punteggio più alto.
-let risultato;
-if (userNumber > pcNumber) {
-    risultato = ("hai vinto")
-} else if(pcNumber > userNumber) {
-    risultato = ("hai perso");
-} else if (userNumber === pcNumber) {
-    risultato = ("pareggio")
-}
-console.log(risultato);
+
+const gameStart = document.getElementById("start");
+
+
+gameStart.addEventListener("click", 
+
+    function() {
+
+    // Generare un numero random da 1 a 6 per il giocatore 
+        const userNumber = Math.floor(Math.random() * 6) +1;
+        console.log(userNumber);
+
+    // Generare un numero random da 1 a 6 per il computer. 
+        const pcNumber = Math.floor(Math.random() * 6) +1;
+        console.log(pcNumber);
+
+
+    // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+        let risultato;
+
+        if (userNumber > pcNumber) {
+        risultato = ("Hai vinto")
+        } else if(pcNumber > userNumber) {
+        risultato = ("Hai perso");
+        } else if (userNumber === pcNumber) {
+        risultato = ("Pareggio");
+        }
+        
+        console.log(risultato);
+
+        document.getElementById("col-user").innerHTML = `Il tuo numero: ${userNumber}`;
+
+        document.getElementById("col-result").innerHTML = risultato;
+
+        document.getElementById("col-pc").innerHTML = `Numero sfidante: ${pcNumber}` ;
+    }
+)
+
 
 
