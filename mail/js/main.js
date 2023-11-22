5c34f95be9bb6dc94da95748c0a25cd4df842ddf
Luca Garizio <garizioluca2@gmail.com>
2023-11-22 16:49:43 +0100

    EX mail feat: add js const usermail & mail allowed

--- mail/js/main.js
+++ mail/js/main.js
@@ -7,12 +7,12 @@
 
 
 // Chiedi all’utente la sua email
-
-
+const userMail = prompt ("Inserisci la tua mail")
+console.log(userMail);
 
 
 // controlla che sia nella lista di chi può accedere
-
+const mailsAllowed = ["mail1@gmail.com", "mail2@gmail.com", "mail3@gmail.com", "mail4@gmail.com", "mail4@gmail.com"];
 
 
 
