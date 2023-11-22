682a7a8b4fcd7845f971ee1b37ba75cc4c9a379f
Luca Garizio <garizioluca2@gmail.com>
2023-11-22 16:38:11 +0100

    EX: mail  config:  set logic base on js file

---
+++ mail/js/main.js
@@ -0,0 +1,19 @@
+// Mail
+
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+
+
+
+
+// Chiedi all’utente la sua email
+
+
+
+
+// controlla che sia nella lista di chi può accedere
+
+
+
+
+// stampa un messaggio appropriato sull’esito del controllo
