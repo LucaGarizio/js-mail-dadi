2b34d299a036e4ea891b75c20500476fcb09bac3
Luca Garizio <garizioluca2@gmail.com>
2023-11-22 19:27:14 +0100

    merge

--- mail/js/main.js
+++ mail/js/main.js
@@ -4,21 +4,29 @@
 
 
 
-
-
 // Chiedi all’utente la sua email
 const userMail = prompt ("Inserisci la tua mail")
 console.log(userMail);
 
 
 // lista di chi può accedere
-const mailsAllowed = ["mail1@gmail.com", "mail2@gmail.com", "mail3@gmail.com", "mail4@gmail.com", "mail4@gmail.com"];
+const mailsAllowed = ["mail1@gmail.com", "mail2@gmail.com", "mail3@gmail.com", "mail4@gmail.com", "mail5@gmail.com"];
 
-// controllo e risultato acesso
-if (mailsAllowed.includes(userMail)) {
-    console.log('La tua mail è nella lista!! Benvenuto');
-  } else {
-    console.log('La tua mail non rientra nella lista!! Accesso negato');
-  }
 
-// stampa un messaggio appropriato sull’esito del controllo
+let accessGranted = 0;
+
+// Ciclo for per controllare l'accesso
+for (let i = 0; i < mailsAllowed.length; i++) {
+    if (userMail === mailsAllowed[i]) {
+
+        accessGranted = 1;     
+    }
+}
+
+// Stampa un messaggio appropriato sull'esito del controllo
+if (accessGranted) {
+    alert("Welcome");
+} else {
+    alert("Sorry your email is not on the list.");
+}
+
