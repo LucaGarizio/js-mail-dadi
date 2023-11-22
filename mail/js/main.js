fc4d8e2751116f241b4e85ada50633b35c3639db
Luca Garizio <garizioluca2@gmail.com>
2023-11-22 16:58:28 +0100

    EX mail feat : add if / else logic

--- mail/js/main.js
+++ mail/js/main.js
@@ -11,9 +11,14 @@ const userMail = prompt ("Inserisci la tua mail")
 console.log(userMail);
 
 
-// controlla che sia nella lista di chi può accedere
+// lista di chi può accedere
 const mailsAllowed = ["mail1@gmail.com", "mail2@gmail.com", "mail3@gmail.com", "mail4@gmail.com", "mail4@gmail.com"];
 
-
+// controllo e risultato acesso
+if (mailsAllowed.includes(userMail)) {
+    console.log('La tua mail è nella lista!! Benvenuto');
+  } else {
+    console.log('La tua mail non rientra nella lista!! Accesso negato');
+  }
 
 // stampa un messaggio appropriato sull’esito del controllo
